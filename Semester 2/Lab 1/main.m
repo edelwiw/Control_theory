ed930b966cc5bf56d541ce9ad359ab5908f230cb
Alex Ivanov <Edelwiw@gmail.com>
2025-02-08 12:02:42 +0300

    Lab 1 upd

--- Semester 2/Lab 1/main.m
+++ Semester 2/Lab 1/main.m
@@ -1,3 +1,34 @@
+function U = get_control_matrix(A, B) 
+    U = [B, A * B, A^2 * B];
+end
+
+function s = check_state_controllability(A, B, x1)
+    U = get_control_matrix(A, B);
+    U_hat = [U, x1];
+    s = rank(U) == rank(U_hat);
+end
+
+function P = get_gramian(A, B, mode, t1)
+    sys = ss(A, B, [], []);
+    opt = gramOptions('TimeIntervals', [0, t1]);
+    P = gram(sys, mode, opt);
+end
+
+function [t, u] = get_control(A, B, x1, t1, num_points)
+    U = get_control_matrix(A, B);
+    P = get_gramian(A, B, 'c', t1);
+    if check_state_controllability(A, B, x1) == 0
+        fprintf(2, "System is not state controllable\n");
+        return;
+    end
+    u_func = @(tau) B' * expm(A' * (t1 - tau)) * pinv(P) * x1;
+    t = linspace(0, t1, num_points);
+    u = zeros(1, length(t));
+    for i = 1:length(t)
+        u(i) = u_func(t(i));
+    end
+end
+
 %% TASK 1 
 
 A = [5, -2, 8; 
@@ -7,69 +38,50 @@ B = [-7; -5; 7];
 x1 = [-2; -3; 3];
 t1 = 3;
 
-%% control matrix 
-U = [B, A * B, A^2 * B];
-fprintf("Control matrix \nU = \n");
-print_matrix(U, 0);
+U = get_control_matrix(A, B);
+fprintf("\nControl matrix \nU = ");
+print_matrix(U, 0); 
+fprintf("\nRank of control matrix: %d\n\n", rank(U));
 
-fprintf("Rank of control matrix: %d\n\n", rank(U));
+P = get_gramian(A, B, 'c', t1);
+fprintf("\nGramian matrix \nP(%d) = ", t1);
+print_matrix(P, 2);
+fprintf("\n");
 
-% gramian 
+[t, u] = get_control(A, B, x1, t1, 1000);   
+plotter({{t, u, "Control signal"}}, "media/plots/task1_control_signal.png", "t", "u", "");
+
 sys = ss(A, B, [], []);
-
-opt = gramOptions('TimeIntervals', [0, t1]);
-gramian = gram(sys, 'c', opt);
-print_matrix(gramian, 2);
-
-
-%% control 
-u_func = @(tau) B' * expm(A' * (t1 - tau)) * inv(gramian) * x1;
-
-% simulation
-t = 0:0.01:t1;
-u = zeros(1, length(t));
-for i = 1:length(t)
-    u(i) = u_func(t(i));
-end
-
 [y, t, x] = lsim(sys, u, t);
 
-plotter({{t, u, "Control signal"}}, "media/plots/task1_control_signal.png", "t", "u", "");
 plotter({{t, x(:, 1), "x_1"}, {t, x(:, 2), "x_2"}, {t, x(:, 3), "x_3"}}, "media/plots/task1_states.png", "t", "x", "");
 fprintf("final state: %.2f %.2f %.2f\n", x(end, 1), x(end, 2), x(end, 3));
 
-
 %% TASK 2
+
+A = [5, -2, 8; 
+    4, -3, 4; 
+    -4, 0, -7];
 B = [-1; -3; 3];
 x1 = [-2; -3; 3];
+x2 = [-3; -3; 4];
+t1 = 3;
 
-U = [B, A * B, A^2 * B];
-fprintf("Control matrix \nU = \n");
-print_matrix(U, 0);
-fprintf("Rank of control matrix: %d\n\n", rank(U));
-
-sys = ss(A, B, [], []);
-
-opt = gramOptions('TimeIntervals', [0, t1]);
-gramian = gram(sys, 'c', opt);
-print_matrix(gramian, 2);
-
-%% 
-U_hat = [U, x1];
-fprintf("rank of U_hat: %d\n", rank(U_hat));
-
-%% control 
-u_func = @(tau) B' * expm(A' * (t1 - tau)) * inv(gramian) * x1;
-
-% simulation
-t = 0:0.01:t1;
-u = zeros(1, length(t));
-for i = 1:length(t)
-    u(i) = u_func(t(i));
+if check_state_controllability(A, B, x1) == 1 
+    fprintf("System is state controllable with x1\n");
+else if check_state_controllability(A, B, x2) == 1
+    fprintf("System is state controllable with x2\n");
+    x1 = x2;
+else
+    fprintf(2, "System is not state controllable\n");
+end 
 end
 
+[t, u] = get_control(A, B, x1, t1, 1000);
+plotter({{t, u, "Control signal"}}, "media/plots/task2_control_signal.png", "t", "u", "");
+
+sys = ss(A, B, [], []);
 [y, t, x] = lsim(sys, u, t);
 
-plotter({{t, u, "Control signal"}}, "media/plots/task2_control_signal.png", "t", "u", "");
 plotter({{t, x(:, 1), "x_1"}, {t, x(:, 2), "x_2"}, {t, x(:, 3), "x_3"}}, "media/plots/task2_states.png", "t", "x", "");
 fprintf("final state: %.2f %.2f %.2f\n", x(end, 1), x(end, 2), x(end, 3));
--- Semester 2/Lab 1/print_matrix.m
+++ Semester 2/Lab 1/print_matrix.m
@@ -3,13 +3,22 @@ function print_matrix(A, precision)
     fprintf("\\begin{bmatrix}\n");
     for i = 1:m
         for j = 1:n
-            if j == n
-                fprintf('%.*f', precision, A(i, j));
-                break;
+
+            if imag(A(i, j)) == 0
+                fprintf('%.*f ', precision, A(i, j));
+            else 
+                if imag(A(i, j)) < 0
+                    fprintf('%.*f - %.*fj ', precision, real(A(i, j)), precision, -imag(A(i, j)));
+                else
+                    fprintf('%.*f + %.*fj ', precision, real(A(i, j)), precision, imag(A(i, j)));
+                end
             end
-            fprintf('%.*f & ', precision, A(i, j));
+            
+            if j < n
+                fprintf(' & ');
+             end
         end
-        fprintf(' \\\\ \n');
+        fprintf('\\\\ \n');
     end
     fprintf("\\end{bmatrix}\n");
 end
