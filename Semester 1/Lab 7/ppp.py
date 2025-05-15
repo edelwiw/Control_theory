09b43fbf15e2bb691ffd8ddfe230d768c41937fb
Alex Ivanov <Edelwiw@gmail.com>
2025-05-16 01:05:44 +0300

    Chapter 1 upd

--- Semester 1/Lab 7/ppp.py
+++
@@ -1,9 +0,0 @@
-s = "task2_p_controller_const_speed.txt task2_p_controller_const_target.txt task2_pi_controller_const_acceleration.txt  task2_pi_controller_const_speed.txt task2_special_controller_wave.txt"
-arr = s.split()
-
-delay_arr = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
-for i in range(len(delay_arr)):
-    # print(f"scp robot@192.168.2.3:~/{i} {i}")
-    # print(f"scp robot@192.168.2.3:~/freq_response_{i}.txt freq_response_{i}.txt")
-    d = delay_arr[i]
-    print(f"scp robot@192.168.2.3:~/P_controller_delayed_{d}.txt P_controller_delayed_{d}.txt")
